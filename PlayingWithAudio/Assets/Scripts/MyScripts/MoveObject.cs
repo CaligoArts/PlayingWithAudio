1ea1318dce83a99842eac59284bf3a502f5b6985
CaligoArts <88352999+CaligoArts@users.noreply.github.com>
2021-08-29 12:17:41 -0400

    Reorganized Scripts & corrected VS output issue
    Removed BottleFly.cs and changed it to MoveObject.cs to make purpose more clear.
    Visual Studio was giving Output error : Can't find file Assembly-CSharp-Editor.csproj so renamed the Assembly-CSharp.csproj that was there to include "Editor" and it resolved.

--- PlayingWithAudio/Assets/Scripts/MyScripts/MoveObject.cs
+++ PlayingWithAudio/Assets/Scripts/MyScripts/MoveObject.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BottleFly : MonoBehaviour
+public class MoveObject : MonoBehaviour
 {
     public Vector3 positionChange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,9 @@ public class BottleFly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += positionChange;
+        //Use public positionChange variable to move object along axis set in editor.
+        {
+            transform.position += positionChange;
+        }
     }
 }
